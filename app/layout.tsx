47210416ac87ab903dc0abf3edb87ff99a18095d
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-17 15:09:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,14 @@ export const metadata = {
   title: "FormFix Pro",
   description: "AI fitness platform"
 };
-
+// app/layout.tsx
+export const metadata = {
+  title: 'FormFix Pro v2',
+  description: 'Professional Tailored Fitness Plans',
+  icons: {
+    icon: 'https://cdn-icons-png.flaticon.com/512/2964/2964514.png', // Možeš zameniti link svojom ikonicom
+  },
+}
 export default function RootLayout({
   children,
 }: {
