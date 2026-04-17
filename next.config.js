b7de5942c13e12f62e130f56f834475e054afdb0
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-17 10:51:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Omogućava statički export (pravi 'out' folder koji GitHub Pages traži)
   output: 'export',
-
-  // 2. Putanja na GitHubu: pošto ti je repo 'app', link je /app/
-  // VAŽNO: Ako ikada promeniš ime repozitorijuma, moraš i ovo promeniti
   basePath: '/app',
-
-  // 3. Isključuje optimizaciju slika jer GitHub Pages nema server za to
+  // Uklonili smo assetPrefix jer .nojekyll radi posao za stilove
+  // a prefix nekad blokira JS skripte
   images: {
     unoptimized: true,
   },
-
-  // 4. (Opciono) Sprečava greške sa koso crtom na kraju URL-a
-  trailingSlash: true,
 };
 
 export default nextConfig;
