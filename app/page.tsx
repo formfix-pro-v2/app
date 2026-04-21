dbc20c8e5082eee2117829ac6c5890134bada9b6
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 14:30:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import PrimaryButton from "../../components/PrimaryButton";
+// Koristimo @/ da bismo gađali root folder direktno
+import PrimaryButton from "@/components/PrimaryButton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#0a0a0a] text-white p-8">
       <main className="flex flex-col gap-8 items-center text-center">
-        {/* Putanja do slike mora imati /app/ ispred zbog GitHub Pages */}
         <Image
-          className="dark:invert"
           src="/app/next.svg"
-          alt="Next.js logo"
+          alt="Logo"
           width={180}
           height={38}
           priority
         />
-        
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <h1 className="text-4xl font-bold mt-4">FormFix Pro v2</h1>
+        <div className="mt-8">
           <Link href="/quiz">
             <PrimaryButton text="Start Quiz" />
           </Link>
