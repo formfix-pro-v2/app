2ab32b7b575063929c1f8f4997c755fffc22b514
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 12:04:44 +0200

    Delete app/sitemap.ts

--- app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-export default function sitemap() {
-  return [
-    { url: "https://formfix-pro-v2.github.io/app/" },
-    { url: "https://formfix-pro-v2.github.io/app/login/" },
-    { url: "https://formfix-pro-v2.github.io/app/privacy/" },
-    { url: "https://formfix-pro-v2.github.io/app/terms/" },
-  ];
-}
