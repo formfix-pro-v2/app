c4890a502ca0ced1ab32d552de65c0784bed0682
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 14:12:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import PrimaryButton from "../components/PrimaryButton";
+import PrimaryButton from "../../components/PrimaryButton";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 items-center text-center">
+        {/* Putanja do slike mora imati /app/ ispred zbog GitHub Pages */}
+        <Image
+          className="dark:invert"
+          src="/app/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link href="/quiz">
             <PrimaryButton text="Start Quiz" />
