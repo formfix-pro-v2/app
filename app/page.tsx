578a5bf64cc4007403e2079d2814f053b2952243
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 13:25:39 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import PrimaryButton from "./components/PrimaryButton";
+import PrimaryButton from "./Components/primaryButton";
 
 export default function Home() {
   return (
