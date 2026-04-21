e7c5f9aa494c602dc886119df247d37da7bcb265
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 14:12:41 +0200

    Update page.tsx

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import PrimaryButton from "../../components/PrimaryButton";
+import PrimaryButton from "../../../components/PrimaryButton";
 
 export default function ResultsPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
-      <h1 className="text-3xl font-bold mb-4">Your Results</h1>
+      <h1 className="text-3xl font-bold mb-4">Vaši rezultati</h1>
+      <p className="mb-8 text-gray-400">Čestitamo na završenom testu!</p>
       <Link href="/">
-        <PrimaryButton text="Go Back Home" />
+        <PrimaryButton text="Nazad na početnu" />
       </Link>
     </div>
   );
