778b6970d3e30f7250bdc3cd196e16b8614275b0
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 14:59:13 +0200

    Update PrimaryButton.tsx

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -10,7 +10,7 @@ export default function PrimaryButton({ text, onClick }: PrimaryButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-black text-white gap-2 hover:bg-[#383838] text-sm h-10 px-5 min-w-[120px]"
+      className="rounded-full bg-white text-black font-bold py-3 px-10 text-base transition-all hover:bg-blue-600 hover:text-white active:scale-95 shadow-lg shadow-white/5 min-w-[160px]"
     >
       {text}
     </button>
