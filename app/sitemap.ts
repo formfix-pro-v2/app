4208e8e89d3727e99e6362f4ba276d5856b60bf1
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 11:00:45 +0200

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://formfix-pro-v2.github.io/app/" },
+    { url: "https://formfix-pro-v2.github.io/app/login/" },
+    { url: "https://formfix-pro-v2.github.io/app/privacy/" },
+    { url: "https://formfix-pro-v2.github.io/app/terms/" },
+  ];
+}
