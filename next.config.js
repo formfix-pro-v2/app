9bce4df8c71a0772bc2e4403d8f9a54c3b3315be
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-21 14:10:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Obavezno za GitHub Pages
-  images: {
-    unoptimized: true, // Obavezno da bi slike radile besplatno
-  },
-  // Ako ti je ime repozitorijuma "app", dodaj ovo:
-  basePath: '/app', 
+  output: 'export',
+  basePath: '/app',
   assetPrefix: '/app',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
