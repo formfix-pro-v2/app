dd26d3c67428b478b389a45bcc3c6e184a9f1a77
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-23 15:14:16 +0200

    Update programs.ts

--- lib/programs.ts
+++ lib/programs.ts
@@ -40,7 +40,7 @@ export const plans: DayPlan[] = [
         start: "Lie on back, knees bent.",
         end: "Lift hips upward.",
         reps: "3 x 12",
-        image: "/app/exercises/bridge.jpg",
+        image: "/app/public/exercises/bridge.jpg",
       },
     ],
   },
